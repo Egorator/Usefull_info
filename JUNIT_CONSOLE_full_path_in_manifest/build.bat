d831ab0ef9e49373b450cfb661e3ba42ed39f3ae
Egor <Egorator1996@gmail.com>
2016-09-08 00:45:52 +0300

    initial commit

---
+++ JUNIT_CONSOLE_full_path_in_manifest/build.bat
@@ -0,0 +1,11 @@
+del Home\Egor\TestRunner2.class Home\Egor\TestAssertions.class Test.jar
+
+javac -cp C:\JUnit\junit-4.10.jar;C:\JUNIT_WORKSPACE\Home\Egor Home\Egor\TestRunner2.java Home\Egor\TestAssertions.java || pause && exit \b
+
+::java -cp C:\JUnit\junit-4.10.jar;. Home.Egor.TestRunner2
+
+jar cvfm Test.jar Manifest.mf Home\Egor\TestAssertions.class Home\Egor\TestRunner2.class || pause && exit \b
+java -jar Test.jar
+
+::YOUR_CMD || pause && exit \b
+pause
